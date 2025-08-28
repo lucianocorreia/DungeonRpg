de17f70a63d931d2c994397cf4647f89d821ba4b
Luciano Correia <luciano.correia@gmail.com>
2025-08-28 10:32:42 +0400

    feat: Implement player attack combo system

--- Scenes/Characters/Player/PlayerAttackState.cs
+++ Scenes/Characters/Player/PlayerAttackState.cs
@@ -4,13 +4,24 @@ using System;
 
 public partial class PlayerAttackState : PlayerState
 {
+    [Export] private Timer comboTimerNode;
+
     private int comboCounter = 1;
     private int maxComboCount = 2;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        comboTimerNode.Timeout += () => comboCounter = 1;
+    }
 
     protected override void EnterState()
     {
         characterNode.AnimationPlayerNode.Play(
-            GameConstants.ANIMATION_ATTACK + comboCounter
+            GameConstants.ANIMATION_ATTACK + comboCounter,
+            -1,
+            1.5f
         );
 
         characterNode.AnimationPlayerNode.AnimationFinished += OnAttackAnimationFinished;
@@ -19,6 +30,7 @@ public partial class PlayerAttackState : PlayerState
     protected override void ExitState()
     {
         characterNode.AnimationPlayerNode.AnimationFinished -= OnAttackAnimationFinished;
+        comboTimerNode.Start();
     }
 
     private void OnAttackAnimationFinished(StringName animName)
@@ -28,4 +40,5 @@ public partial class PlayerAttackState : PlayerState
 
         characterNode.StateMachineNode.SwitchState<PlayerIdleState>();
     }
+
 }
